4aea1fa78f61b2aa44fa25b76996b458d1db9cd9
Samuel Amankwah Donkor <56901167+samadon1@users.noreply.github.com>
2020-07-27 20:38:25 -0700

    Update main_covid19.py

--- main_covid19.py
+++ main_covid19.py
@@ -93,7 +93,7 @@ headModel = AveragePooling2D(pool_size=(4, 4))(headModel)
 headModel = Flatten(name="flatten")(headModel)
 headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
-headModel = Dense(1, activation="sigmoid")(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
 
 # place the head FC model on top of the base model (this will become
 # the actual model we will train)
